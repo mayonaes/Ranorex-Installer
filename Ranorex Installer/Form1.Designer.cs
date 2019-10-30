45f9ae98ae7b3a484c5911f1345264da4dd2b0d3
Sean Perrotta <sperrotta@ranorex.com>
2019-10-30 16:00:35 -0400

    closes #1 - Resized gui window

--- Ranorex Installer/Form1.Designer.cs
+++ Ranorex Installer/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(692, 496);
+            this.ClientSize = new System.Drawing.Size(407, 256);
             this.Name = "Form1";
             this.Text = "Ranorex Installer";
             this.ResumeLayout(false);
